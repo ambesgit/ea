c49432ea859e48938c799ff9675bc0c214133288
ambesgit <ambestetemke@gmail.com>
2016-01-19 23:33:56 -0600

    w2d2 SDI3

---
+++ SDI3/src/main/java/cs544/sdi3/BookService.java
@@ -0,0 +1,43 @@
+package cs544.sdi3;
+
+import java.util.*;
+
+public class BookService implements IBookService {
+	//public List<IBookSupplier> suppliers = new ArrayList<IBookSupplier>();
+        private List<IBookSupplier> suppliers;        
+	public BookService(List<IBookSupplier> suppliers) {
+            this.suppliers=suppliers;
+            /*
+		IBookSupplier amazon = new Amazon();
+		IBookSupplier barnesandnoble = new BarnesAndNoble();
+		IBookSupplier ebooks = new EBooks();
+                
+		suppliers.add(amazon);
+		suppliers.add(barnesandnoble);
+		suppliers.add(ebooks);
+                */
+	}
+
+	public void buy(Book book) {
+		double lowestPrice = 0;
+		IBookSupplier cheapestSupplier = null;
+		// find the cheapest supplier
+		for (IBookSupplier supplier : suppliers) {
+			double price = supplier.computePrice(book.getIsbn());
+			if (cheapestSupplier == null) {
+				cheapestSupplier = supplier;
+				lowestPrice = price;
+			} else {
+				if (price < lowestPrice) {
+					cheapestSupplier = supplier;
+					lowestPrice = price;
+				}
+			}
+		}
+		// buy with the cheapest supplier
+		if (cheapestSupplier != null) {
+			cheapestSupplier.order(book);
+		}
+
+	}
+}
---
+++ SDI3/src/main/java/cs544/sdi3/IBookService.java
@@ -0,0 +1,5 @@
+package cs544.sdi3;
+
+public interface IBookService {
+    public void buy(Book book);
+}
