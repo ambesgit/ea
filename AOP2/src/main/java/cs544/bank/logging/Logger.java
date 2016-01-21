e7d5efa76397244244d4c2140a7f2852c78a3fd6
ambesgit <ambestetemke@gmail.com>
2016-01-20 23:36:11 -0600

    w2d3 AOP2 Updated

--- AOP2/src/main/java/cs544/bank/logging/Logger.java
+++ AOP2/src/main/java/cs544/bank/logging/Logger.java
@@ -1,9 +1,36 @@
 package cs544.bank.logging;
 
-public class Logger implements ILogger{
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.util.StopWatch;
 
+@Aspect
+public class Logger implements ILogger{
+        
 	public void log(String logstring) {
 		java.util.logging.Logger.getLogger("BankLogger").info(logstring);		
 	}
+        @After("execution(* cs544.bank.dao.*.*(..))")
+        public void loggingAllMethodsInBankDAO(JoinPoint joinPoint){
+            log("Logging from package bank.dao:"+" method="+joinPoint.getSignature().getName());
+        }
+        @Around("execution(* cs544.bank.service.*.*(..))")
+        public Object getExecutionTime(ProceedingJoinPoint call) throws Throwable{
+            StopWatch sw=new StopWatch("");
+            sw.start(call.toShortString());
+            Object reTurn=call.proceed();
+            sw.stop();
+            long tTime=sw.getLastTaskTimeMillis();
+            log("From bank service package: "+" ,Method="+call.getSignature().getName()+" ,totalTimeOf execution="+tTime);
+            return reTurn;  
+        }
+        @After("execution(* cs544.bank.jms.*.*(..))&& args(text)")
+        public void getEveryJMS(JoinPoint joinPoint,String text){
+            System.out.println("XXXXXXXXXXXXLook JMSMESSAGE BELOW THIS LINEXXXXXXXXXXXXXXXX");
+                log(text);
+        }
 
 }
